b9f6bbd7d06f676f7b91ab5308751e63ebdcc722
mua900 <mao090001@gmail.com>
2026-07-13 19:54:01 +0300

    add planet surface mode to select mission location

---
+++ asset/shader/src/planet_surface.hlsl
@@ -0,0 +1,26 @@
+#include "open_simplex.hlsl"
+
+cbuffer SurfaceInfo : register(b0, space3)
+{
+	// @todo
+};
+
+cbuffer CameraInfo : register(b1, space3)
+{
+    float4 camera;
+    float2 offset;
+    float2 padding;
+};
+
+struct PixelShaderInput
+{
+    float4 pos : SV_POSITION;
+    float4 color : COLOR0;
+    float2 tex : TEXCOORD0;
+};
+
+float4 main(PixelShaderInput input) : SV_TARGET
+{
+	// @todo
+    return float4(1, 0, 0, 1);
+}
